c68e6b961e887125cc7e6695afeeca26dc83c904
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2021-12-02 17:12:59 +0700

    create database

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd9ed5e4ca9ae985c220f828f3c937efc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '01693424-d354-4d19-9779-7d2874e842ce'
+};
